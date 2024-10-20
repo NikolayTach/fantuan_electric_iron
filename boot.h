8ca911f8d0da8516059a2f2bfe8e30ca20e99403
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2024-10-20 13:15:49 -0400

    Add files via upload

---
+++ boot.h
@@ -0,0 +1,3 @@
+#include"stdio.sh"
+
+BOOT_IMAGE=(hd1,gpt2)/vmlinuz-6.8.5-301.fc40.x86_64
---
+++ tp.py
@@ -0,0 +1,3 @@
+import create as E_STRING
+def create():
+    print("create,@introversion_style,@feed_application&")
